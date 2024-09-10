1947af472e91f8f90f756b4ccbdf004079a2a8a8
shashwatpal1021 <shashwatpal1021@gmail.com>
2024-09-10 15:37:03 +0530

    done

--- packages/ui/src/signup.tsx
+++ packages/ui/src/signup.tsx
@@ -7,16 +7,17 @@ export const Signup = ({ children }: { children: React.ReactNode }) => {
   // const [email, setEmail] = useState('');
   // const [password, setPassword] = useState('');
   // const [confirmPassword, setConfirmPassword] = useState('');
-  const [fomrData,setformData]=useState({
-    email:'',
-    password:'',
-    confirmPassword:'',
+  const [fomrData, setformData] = useState({
+    name: "",
+    email: '',
+    password: '',
+    confirmPassword: '',
   })
 
-  const setData=(e:any)=>{
+  const setData = (e: any) => {
     setformData({ ...fomrData, [e.target.name]: e.target.value })
   }
- 
+
   const SubmitData = async () => {
     const result = signUpSchema.safeParse(fomrData);
     if (result.success) {
@@ -29,14 +30,42 @@ export const Signup = ({ children }: { children: React.ReactNode }) => {
   }
   return (
 
-    <div>
-      <h1>Hey there! Welcome</h1>
-      {children}
-      <br />
-      <label htmlFor="email">Email: <input type="text" value={fomrData.email} name="email" id="email" onChange={setData} /></label>
-      <label htmlFor="createpassword">Create Password: <input type="text" value={fomrData.password} name="createpassword" id="createpassword" onChange={setData} /></label>
-      <label htmlFor="confirmpassword">Confirm Password: <input type="text" value={fomrData.confirmPassword} name="confirmpassword" id="confirmpassword"   onChange={setData} /></label>
-      <button type="submit" onClick={SubmitData}>Submit</button>
+    <div className="flex flex-col justify-center ">
+      <div className="flex flex-col p-5 justify-center border-solid border-2  rounded-lg shadow-2xl">
+        <div className="text-2xl mb-1 font-bold">{children}</div>
+        <h1>Hey there! Welcome</h1>
+        <hr />
+        <label htmlFor="name" className="p-1">Name: <input type="text"
+          className="p-1 w-full border-2 border-black rounded-lg"
+          value={fomrData.name}
+          name="name"
+          id="name"
+          placeholder="Name"
+          onChange={setData} /></label>
+        <label htmlFor="email" className="p-1">Email: <input type="text"
+          className="p-1 w-full border-2 border-black rounded-lg"
+          value={fomrData.email}
+          name="email"
+          id="email"
+          placeholder="email"
+          onChange={setData} /></label>
+        <label htmlFor="password" className="p-1">Paswword: <input type="text"
+          className="p-1 w-full border-2 border-black rounded-lg"
+          value={fomrData.password}
+          name="password"
+          id="password"
+          placeholder="password"
+          onChange={setData} /></label>
+        <label htmlFor="confirmpassword" className="p-1">Confirm Password: <input type="text"
+          className="p-1 w-full border-2 border-black rounded-lg"
+          value={fomrData.confirmPassword}
+          name="confirmpassword"
+          id="confirmpassword"
+          placeholder="confirmpassword"
+          onChange={setData} /></label>
+        <label htmlFor="confirmpassword" className="pt-3 pr-1 pl-1"><button type="submit" className="p-1 w-full border-2 border-black rounded-lg" onClick={SubmitData}>Submit</button>
+        </label>
+      </div>
     </div>
   );
 };
