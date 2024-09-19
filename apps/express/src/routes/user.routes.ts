e5eef8b86448d29252ecacb7c1fa090baf1f9fd8
Shashwat Pal <shashwatpal1021@gmail.com>
2024-09-19 17:31:00 +0530

    Shashwat (#12)
    * branch add check
    
    * data
    
    * structure backed signup api
    
    * added sign-in api

--- apps/express/src/routes/user.routes.ts
+++ apps/express/src/routes/user.routes.ts
@@ -1,9 +1,7 @@
 import Express, { Request, Response } from "express";
 import { PrismaClient } from "@repo/db/client";
 //@ts-ignore
-import { signUpSchema } from "@repo/ui/schema/signup";
-//@ts-ignore
-import app from "../app/app";
+import { signUpSchema, signInSchema } from "@repo/ui/schema/signup";
 
 const prisma = new PrismaClient();
 
@@ -39,5 +37,34 @@ userRouter.post('/create', async (req: any, res: any) => {
     res.status(500).json({ message: 'Something went wrong' })
   }
 })
+
+userRouter.post("/sign-in", async (req: Request, res: Response) => {
+  console.log("res", req.body)
+  try {
+    const result = signInSchema.safeParse(req.body)
+    const user = await prisma.user.findUnique({
+      where: {
+        email: result.data.email
+      }
+    })
+    if (!user) {
+      return res.status(400).json({
+        message: 'User not found'
+      })
+    } 
+    if (user.password !== result.data.password) {
+      return res.status(400).json({
+        message: 'Incorrect password'
+      })
+    } 
+    res.status(200).json({
+      message: 'User logged in successfully',
+      data: user
+    })  
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
 
 export default userRouter;
